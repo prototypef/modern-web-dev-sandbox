77aec74e11ff24f6ff7d7466dfc0353730e127d8
Fernando Berrios <fberrios@gmail.com>
2013-09-11 17:24:34 -0500

    Added grunt-usemin dependencies and configuration. Adjusted index.html to use usemin script block.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,8 @@
 module.exports = function(grunt) {
   'use strict';
 
-  // Declare JS dependencies and third-party libraries required
-  var jsVendorSourceFiles = [
-    'bower_components/jquery/jquery.js',
-    'bower_components/lodash/dist/lodash.js',
-    'bower_components/handlebars/handlebars.js',
-    'bower_components/ember/ember.js',
-    'bower_components/accounting/accounting.js',
-    'bower_components/moment/moment.js'
-  ];
-
   // Declare the App's source files
-  var jsAppSourceFiles = [
-    'js-src/app.js',
-    'js-src/views/login.js'
-  ];
-
-  // Concatenate our file arrays to create a bundle we can use later
-  var jsSourceFiles = jsVendorSourceFiles.concat(jsAppSourceFiles);
+  var jsAppSourceFiles = grunt.file.expand('js-src/**/*.js');
 
   // Project configuration.
   grunt.initConfig({
@@ -49,51 +33,42 @@ module.exports = function(grunt) {
         }
       }
     },
-    uglify: {
-      dist: {
-        options: {
-          sourceMap: 'js/source.map',
-          sourceMappingURL: '/js/source.map',
-          sourceMapRoot: '/'
-        },
-        files: [
-          {
-            src: jsSourceFiles,
-            dest: 'js/app.js'
-          }
-        ]
-      }
-    },
     watch: {
       styles: {
         files: ['sass/*.scss'],
         tasks: ['compass:dev']
-      },
-      scripts: {
-        files: jsAppSourceFiles,
-        tasks: ['uglify:dist']
       }
+    },
+    useminPrepare: {
+      html: 'index.html'
+    },
+    usemin: {
+      html: 'index.html'
     }
   });
 
   // Load the plugin tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-usemin');
 
   // Default task(s).
   grunt.registerTask('default', [
     'jshint',
     'compass:clean',
-    'compass:dev',
-    'uglify:dist'
+    'compass:dev'
   ]);
 
   grunt.registerTask('build-prod', [
     'jshint',
     'compass:clean',
     'compass:prod',
-    'uglify:dist'
+    'useminPrepare',
+    'concat',
+    'uglify',
+    'usemin'
   ]);
 };
